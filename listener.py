bbb8caf4a504dc94f6dad652a6625160e652018d
radiance0718 <3362985694@qq.com>
2023-03-18 00:13:36 +0800

    wdh commit 4

--- listener.py
+++ listener.py
@@ -2,7 +2,6 @@ import math
 
 from robot import Robot
 from robot import RobotState
-from robot import RobotCommand
 from edge import Edge
 from workbench import Workbench
 from path_planning import PathPlanning
@@ -10,12 +9,13 @@ from robot_control import RobotControl
 
 class MyListenser:
 
-    def __init__(self, plan: PathPlanning, ctrl: RobotControl):
+    def __init__(self, plan: PathPlanning):
         self.plan = plan
-        self.ctrl = ctrl
+        self.rob : [Robot] = self.plan.get_robots()
+        self.bench : [] = self.plan.get_workbench()
+        self.near = [0, 0, 0, 0, 0]
 
-    def check_dis(self, robcom: RobotCommand, bench: Workbench):
-        rob: Robot = robcom.robot
+    def check_dis(self, rob: Robot, bench: Workbench):
         dis: float = math.dist(rob.pos, bench.pos)
         v: float = 0.12
         t: float = dis/v
@@ -25,46 +25,103 @@ class MyListenser:
             return False
 
 
-    def change_robotcommand(self, robcom: RobotCommand, edge: Edge):
-        rob: Robot = robcom.robot
-        if rob.state == RobotState.IDLE and self.check_dis(robcom, edge.relay): #change date to taking
-            robcom.robot.state = RobotState.TAKING
-            robcom.target = edge.relay
-        elif rob.state == RobotState.TAKING and rob.loadingItem is not None: #change date to loading
-            robcom.robot.state = RobotState.LOADING
-            robcom.target = edge.relay
-        elif rob.state == RobotState.LOADING and rob.loadingItem is None: #change date to idle
-            robcom.robot.state = RobotState.IDLE
-        return
-
-
-    def collect(self, rob: [Robot], bench: [Workbench]):
-        frame: int = input()
-        money: int = input()
-        num_bench: int = input()
-        for i in range(1, num_bench+1):
-            garbage = input()
-            garbage = input()
-            garbage = input()
-            bench[i].state = input()
-            ### miss to define whether the materials are ready or not
-            con:int = input()
-            if con == 1:
-                bench[i].container = WorkbenchState.FULL
+    def change_robot_command(self, rc: RobotControl,  rob: Robot, edge: Edge, near : int):
+        if rob.state == RobotState.IDLE:
+            if edge.fo.container == 1:
+                rob.state = RobotState.TAKING
+                return edge.fo.id
             else:
-                bench[i].container = WorkbenchState.EMPTY
+                if self.check_dis(rob, edge.fo):
+                    return edge.fo.id
+                else:
+                    return int(0)
+        elif rob.state == RobotState.TAKING:
+            if rob.itemId == 0 and near == edge.fo.id:
+                rc.buy(rob)
+
+            if rob.itemId != 0:
+                rob.state = RobotState.LOADING
+                return edge.to.id
+        else:
+            if rob.itemId != 0 and near == edge.to.id:
+                rc.sell(rob)
+
+            if rob.itemId == 0:
+                rob.state = RobotState.IDLE
+                return 0
+
+
+
+    def collect(self):
+        p : int = 0
+        frame = int(input())
+        money = int(input())
+        num_bench = int(input())
+
+        for i in range(1, num_bench+1):
+            typ = input()
+            ben_pos : tuple = (input(), input())
+            self.bench[i].state = int(input())
+            self.bench[i].inputs = int(input())
+            self.bench[i].output = int(input())
 
 
         for i in range(1, 5):
-            ### miss to define the nearest workbench
-            rob[i].loadingitem = input() ###need to be change
-            ### miss to define the time and collision coefficient
-            rob[i].w = input()
-            rob[i].vel = (input(), input())
-            rob[i].rot = input()
-            rob[i].pos = (input(), input())
+            l = []
+            for j in range(1, 11):
+                l.append(input())
+            self.near[i] = int(l[0])+1
+            self.rob[i].itemId = int(l[1])
+            self.rob[i].w = float(l[4])
+            self.rob[i].vel = ( float(l[5]), float(l[6]))
+            self.rob[i].rot = float(l[7])
+            self.rob[i].pos = ( float(l[8]), float(l[9]))
+        return frame
+    def interact(self):
+        rc = RobotControl()
+        frame = self.collect()
+        target = [0]
+        for i in range (1, 5):
+            target.append(self.change_robot_command(rc, self.rob[i], self.rob[i].loadingTask, self.near[i]))
 
+        col = rc.collision_pred(self.rob)
+        if col != 0:
+            p1 = 0
+            p2 = 0
+            for i in range(1, 5):
+                if ((1<<i)&col) != 0:
+                    if p1 == 0:
+                        p1 = i
+                    else:
+                        p2 = i
+        rc.collsion_avoid(self.rob[p1], self.rob[p2])
 
+        for i in range (1, 5):
+            if ((1<<i)&col) == 0:
+                rc.forward(self.rob[i], self.bench[target[i]])
+
+        self.plan.update_idle_queue(frame)
+        self.plan.init_task()
+
+    def init_data(self):
+        cnt_rob = 0
+        cnt_ben = 0
+
+        for i in range(1, 101):
+            for j in range(1, 101):
+                c = input()
+                if c == 'A':
+                    cnt_rob += 1
+                    new_rob = Robot(cnt_rob, (float(i-1)*0.5+0.25, 50-0.25-float(i-1)*0.5))
+                    self.rob.append(new_rob)
+                elif c == '.':
+                    pass
+                else:
+                    cnt_ben += 1
+                    new_ben = Workbench(cnt_ben, int(c), (float(i-1)*0.5+0.25, 50-0.25-float(i-1)*0.5))
+                    self.bench.append(new_ben)
+
+        self.plan.init_task()
 
 
 
