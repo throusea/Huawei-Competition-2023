8613f2a677578970fd2bf78dd881c2eb87d249c0
FCKon <FCKong@outlook.com>
2023-03-20 00:09:21 +0800

    changed robot_control again.

--- robot_control.py
+++ robot_control.py
@@ -23,6 +23,21 @@ def angle(p1: tuple, p2: tuple): # p2 -> p1!
         ag = ag + math.pi
     return ag
 
+def resultant_force(forces: [Force]):
+    result=Force(0,0)
+    for f in forces:
+        result.x = result.x+f.x
+        result.y = result.y+f.y
+    return result
+class Force:
+
+    def __init__(self, x:float, y:float):
+        self.x=x
+        self.y=y
+
+    def angle(self):
+        return angle((0,0),(self.x,self.y))
+
 class RobotControl:
     kp_f = 7
     kd_f = 1
@@ -81,79 +96,4 @@ class RobotControl:
 
     def sell(self, robot: Robot):
         print("sell %d" % robot.id)
-
-    def collision_predict(self, robots: [Robot]):
-        result = [[False, False, False, False], [False, False, False, False], [False, False, False, False],
-                  [False, False, False, False]]
-        #if len(self.forward_ban) > 0:
-        #    return result
-        for i in range(1, 26):
-            poses = []
-            for j in range(0, 4):
-                poses.append(mov_predict(robots[j], i))
-            for j in range(0, 3):
-                for k in range(j + 1, 4):
-                    dist = ((poses[j][0] - poses[k][0]) ** 2 + (poses[j][1] - poses[k][1]) ** 2) ** 0.5
-                    if dist < 0.8:
-                        result[j][k] = result[k][j] = True
-                        return result
-        return result
-
-    def collision_avoid(self, robot1: Robot, robot2: Robot):
-        if robot1.state == RobotState.IDLE:
-            return
-        if robot2.state == RobotState.IDLE:
-            return
-
-        if robot1.state != RobotState.DELIVERING and robot2.state != RobotState.DELIVERING:
-            self.forward(robot1)
-            self.forward(robot2)
-            return
-
-        if robot1.state == RobotState.TAKING:
-            bench1 = robot1.loadingTask.fo
-        else:
-            bench1 = robot1.loadingTask.to
-
-        if robot2.state == RobotState.TAKING:
-            bench2 = robot2.loadingTask.fo
-        else:
-            bench2 = robot2.loadingTask.to
-        if robot1.vel < 0.01:
-            self.forward_ban[robot2.id] = True
-            self.last_frame[robot2.id] = 25
-            ag1=angle(robot1.pos, bench1.pos)
-            da1=ag1-robot2.rot
-            if da1 > 0 or da1 < -math.pi/2:
-                self.avoid_type[robot2.id]=-math.pi/2
-            else:
-                self.avoid_type[robot2.id]=math.pi/2
-        elif robot2.vel < 0.01:
-            self.forward_ban[robot1.id] = True
-            self.last_frame[robot1.id] = 25
-            ag2=angle(robot2.pos, bench2.pos)
-            da2=ag2-robot1.rot
-            if da2 > 0 or da2 < -math.pi/2:
-                self.avoid_type[robot1.id]=-math.pi/2
-            else:
-                self.avoid_type[robot1.id]=math.pi/2
-        else:
-            da=robot1.rot-robot2.rot
-            if da > math.pi:
-                da = da-math.pi
-            if da < -math.pi:
-                da = da+math.pi
-            if da < - math.pi*5/6 or da > math.pi*5/6:
-                self.forward_ban[robot1.id]=True
-                self.last_frame[robot1.id]=25
-                self.forward_ban[robot2.id]=True
-                self.last_frame[robot2.id]=25
-                self.avoid_type[robot1.id]=math.pi/2
-                self.avoid_type[robot2.id]=math.pi/2
-            else:
-                self.forward_ban[robot1.id]=True
-                self.last_frame[robot1.id]=25
-                self.avoid_type[robot1.id]=0
-        self.forward(robot1)
-        self.forward(robot2)
 
