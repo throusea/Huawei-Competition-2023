69de412fef2ae9def667ec5edd12c36d90aa3597
radiance0718 <3362985694@qq.com>
2023-04-06 23:52:33 +0800

    myutil with bfs

--- myutil.py
+++ myutil.py
@@ -6,33 +6,46 @@ def dist(s, d):
 def is_in_set(b: int, b_set: int):
     return (b_set & (1<<b)) != 0
 
-vis = np.zeros(100, 100)
+def bfs(i, j, cnt, n):
+    q = []
+    s = 0
+    r = 0
+    q.append((i, j))
+    while(r <= s):
+        x = (int)(q[r][0])
+        y = (int)(q[r][1])
+        r += 1
+        if vis[x][y] == 1 or mmp[x][y] == 1:
+            continue
+        vis[x][y] = 1
+        mp_ret[x][y] = cnt
+        if(x-1 >= 0):
+            s+=1
+            q.append((x-1, y))
+        if (x + 1 < n):
+            s += 1
+            q.append((x+1, y))
+        if (y - 1 >= 0):
+            s += 1
+            q.append((x, y-1))
+        if (y + 1 < n):
+            s += 1
+            q.append((x, y+1))
 
-def dfs(i, j, cnt, mp, n):
-    if vis[i][j] == 1 or mp[i][j] == 1:
-        return
-
-    vis[i][j] = 1
-    mp_ret[i][j] = cnt
-    if i-1 >= 0:
-        dfs(i-1, j, cnt, mp, n)
-    if i+1 < n:
-        dfs(i+1, j, cnt, mp, n)
-    if j-1 >= 0:
-        dfs(i, j-1, cnt, mp, n)
-    if j+1 < n:
-        dfs(i, j+1, cnt, mp, n)
-
-def check_block(n: int, mp: []):
+def check_block(n: int, mp):
+    global vis
+    vis = np.zeros((n, n), dtype=int)
     for i in range(0, n):
         for j in range(0, n):
             vis[i][j] = 0
     cnt = 1
+    global mmp
+    mmp = mp
     global mp_ret
-    mp_ret = np.zeros(n, n)
+    mp_ret = np.zeros((n, n), dtype=int)
     for i in range(0, n):
         for j in range(0, n):
             if vis[i][j] == 0 and mp[i][j] != 1:
                 cnt+=1
-                dfs(i, j, cnt, mp, n)
+                bfs(i, j, cnt, n)
     return mp_ret
